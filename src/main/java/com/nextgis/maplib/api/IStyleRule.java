837017367fb2e8bb9596dd589a6a181d1bc55622
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-06-04 12:19:29 +0200

    Update copyright for authors

--- src/main/java/com/nextgis/maplib/api/IStyleRule.java
+++ src/main/java/com/nextgis/maplib/api/IStyleRule.java
@@ -1,19 +1,24 @@
-/**
- * **************************************************************************** Project:  NextGIS
- * mobile apps for Compulink Purpose:  Mobile GIS for Android Authors:  Dmitry Baryshnikov (aka
- * Bishop), polimax@mail.ru NikitaFeodonit, nfeodonit@yandex.com *****************************************************************************
- * Copyright (C) 2014-2015 NextGIS
- * <p/>
- * This program is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * <p/>
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * <p/>
- * You should have received a copy of the GNU General Public License along with this program.  If
- * not, see <http://www.gnu.org/licenses/>. ****************************************************************************
+/*
+ * Project:  NextGIS Mobile
+ * Purpose:  Mobile GIS for Android.
+ * Author:   Dmitry Baryshnikov (aka Bishop), bishop.dev@gmail.com
+ * Author:   NikitaFeodonit, nfeodonit@yandex.com
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * *****************************************************************************
+ * Copyright (c) 2012-2015. NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nextgis.maplib.api;
