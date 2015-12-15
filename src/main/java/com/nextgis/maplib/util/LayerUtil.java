3376ee0ef74eea11b926c13a4ae4ac67d8e09dc0
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-12-15 15:05:45 +0300

    LayerUtil.normalizeFieldName() -- remove toLowerCase()

--- src/main/java/com/nextgis/maplib/util/LayerUtil.java
+++ src/main/java/com/nextgis/maplib/util/LayerUtil.java
@@ -1,7 +1,29 @@
+/*
+ * Project:  NextGIS Mobile
+ * Purpose:  Mobile GIS for Android.
+ * Author:   Dmitry Baryshnikov (aka Bishop), bishop.dev@gmail.com
+ * Author:   NikitaFeodonit, nfeodonit@yandex.com
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * *****************************************************************************
+ * Copyright (c) 2012-2015. NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.nextgis.maplib.util;
 
 import android.content.Context;
-
 import com.nextgis.maplib.R;
 
 /**
@@ -36,7 +58,7 @@ public class LayerUtil {
         if(result.equals(Constants.FIELD_ID))
             return "_fixed_id";
 
-        return result.toLowerCase();
+        return result;
     }
 
     public static int stringToType(String type)
