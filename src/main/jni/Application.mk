15dc29da30bf4eef4030eb05ac4aceec7f71cd3b
NikitaFeodonit <nikitafeodonit@yandex.com>
2016-07-09 21:24:10 +0300

    Remove cmake install task. Update AS scripts

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -2,6 +2,7 @@
 NDK_TOOLCHAIN_VERSION := 4.9
 # drop mips for now: mips  mips64
 APP_ABI := armeabi armeabi-v7a x86 arm64-v8a x86_64
+#APP_ABI := armeabi-v7a # for debug
 #  Enable C++11. However, pthread, rtti and exceptions aren’t enabled
 APP_CPPFLAGS += -std=c++11
 # Instruct to use the static GNU STL implementation
