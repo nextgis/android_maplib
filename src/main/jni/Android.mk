6100927e7fd23b69747798c4459c58e39341ef09
NikitaFeodonit <nikitafeodonit@yandex.com>
2016-07-14 22:00:39 +0300

    Revert to libngstroreapi

--- src/main/jni/Android.mk
+++ src/main/jni/Android.mk
@@ -8,6 +8,12 @@ LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libngstore.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE    := ngstoreapi
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libngstoreapi.so
+#LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/ngstoreapi
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := ngsandroid
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libngsandroid.so
 #LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include/ngsandroid
