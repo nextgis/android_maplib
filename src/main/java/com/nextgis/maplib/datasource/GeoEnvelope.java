6d2ad6efe7e6643805a8ab912c6b92e12d93935c
Stanislav Petryakov <become.glory@yandex.ru>
2017-01-13 14:14:35 +0300

    Fix GeoEnvelope crash

--- src/main/java/com/nextgis/maplib/datasource/GeoEnvelope.java
+++ src/main/java/com/nextgis/maplib/datasource/GeoEnvelope.java
@@ -5,7 +5,7 @@
  * Author:   NikitaFeodonit, nfeodonit@yandex.com
  * Author:   Stanislav Petriakov, becomeglory@gmail.com
  * *****************************************************************************
- * Copyright (c) 2012-2015. NextGIS, info@nextgis.com
+ * Copyright (c) 2012-2017 NextGIS, info@nextgis.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser Public License as published by
@@ -342,6 +342,9 @@ public class GeoEnvelope
     }
 
     public void set(GeoEnvelope env) {
+        if (env == null)
+            return;
+
         mMinX = env.mMinX;
         mMaxX = env.mMaxX;
         mMinY = env.mMinY;
