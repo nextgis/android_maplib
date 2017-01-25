740797532d962b0a4ee199a4a7fab61fa541434e
Stanislav Petryakov <become.glory@yandex.ru>
2017-01-25 13:02:38 +0300

    Fix crashes

--- src/main/java/com/nextgis/maplib/map/VectorLayer.java
+++ src/main/java/com/nextgis/maplib/map/VectorLayer.java
@@ -5,7 +5,7 @@
  * Author:   NikitaFeodonit, nfeodonit@yandex.com
  * Author:   Stanislav Petriakov, becomeglory@gmail.com
  * *****************************************************************************
- * Copyright (c) 2012-2016 NextGIS, info@nextgis.com
+ * Copyright (c) 2012-2017 NextGIS, info@nextgis.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser Public License as published by
@@ -2081,7 +2081,7 @@ public class VectorLayer
     public List<Field> getFields()
     {
         if (null == mFields) {
-            return null;
+            return new ArrayList<>();
         }
         return new LinkedList<>(mFields.values());
     }
