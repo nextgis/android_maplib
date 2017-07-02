600b707530f630ab6113e6356334b8e58b0bdbe1
NikitaFeodonit <nikitafeodonit@yandex.com>
2017-07-02 22:29:01 +0300

    IProgressor. Add license info. Fix misprint.

--- src/main/java/com/nextgis/maplib/api/IProgressor.java
+++ src/main/java/com/nextgis/maplib/api/IProgressor.java
@@ -1,3 +1,26 @@
+/*
+ * Project:  NextGIS Mobile
+ * Purpose:  Mobile GIS for Android.
+ * Author:   Dmitry Baryshnikov (aka Bishop), bishop.dev@gmail.com
+ * Author:   NikitaFeodonit, nfeodonit@yandex.com
+ * Author:   Stanislav Petriakov, becomeglory@gmail.com
+ * *****************************************************************************
+ * Copyright (c) 2012-2016 NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.nextgis.maplib.api;
 
 /**
@@ -23,7 +46,7 @@ public interface IProgressor {
     void setValue(int value);
 
     /**
-     * If progress cannopt determinate the max value and current value, it can be set as indeterminate.
+     * If progress cannot determinate the max value and current value, it can be set as indeterminate.
      * @param indeterminate true to set indeterminate state, or false
      */
     void setIndeterminate(boolean indeterminate);
