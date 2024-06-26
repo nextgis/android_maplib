64277770d19635b31d2920b4e4a1d533e3661dd8
AlexeyIke <alexey.kovalenko.v@gmail.com>
2024-06-26 12:25:22 +1000

    test fix

--- src/androidTest/java/com/nextgis/maplib/ApplicationTest.java
+++ src/androidTest/java/com/nextgis/maplib/ApplicationTest.java
@@ -24,17 +24,17 @@
 package com.nextgis.maplib;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
 
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest
-        extends ApplicationTestCase<Application>
+//        extends
+//        ApplicationTestCase<Application>
 {
-    public ApplicationTest()
-    {
-        super(Application.class);
-    }
+//    public ApplicationTest()
+//    {
+//        super(Application.class);
+//    }
 }
